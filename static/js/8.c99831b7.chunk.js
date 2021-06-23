04cf3efc27cded91af4d8734dff45c428aa540e5
Philip Heltweg <pheltweg@gmail.com>
2021-06-23 15:07:36 +0200

    Updates

--- static/js/8.c99831b7.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppreference-builder"]=this["webpackJsonppreference-builder"]||[]).push([[8],{32:function(e,t,n){"use strict";n.r(t),n.d(t,"show_signature",(function(){return s}));var c=n(0),r=n(2),u=n(3),i=n(15),s=function(){function e(t){Object(r.a)(this,e),Object(i.f)(this,t)}return Object(u.a)(e,[{key:"render",value:function(){return Object(i.d)(i.a,null,Object(i.d)("div",null,"\u2211 = ","{".concat(Object(c.a)(this.signature).map((function(e){return"".concat(e)})),"}")))}}]),e}();s.style=":host{display:block}"}}]);
-//# sourceMappingURL=8.c99831b7.chunk.js.map
